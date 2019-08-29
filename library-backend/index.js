96f16162974522cb13bb06247e5021858780f5a8
antjuntu <antjuntu@gmail.com>
2019-08-29 15:49:57 +0300

    8.13: Tietokanta, osa 1

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,140 @@
 const { ApolloServer, gql } = require('apollo-server')
-const uuid = require('uuid/v1')
+const mongoose = require('mongoose')
+
+const Book = require('./models/book')
+const Author = require('./models/author')
+
+const { password } = require('./secret')
+const MONGODB_URI = `mongodb+srv://antti:${password}@cluster0-nb9xg.mongodb.net/library-backend?retryWrites=true&w=majority`
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+console.log('connecting to', MONGODB_URI)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connectiong to MongoDB', error.message)
+  })
+
+
+const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(
+      author: String
+      genre: String
+    ): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+`
+
+const resolvers = {
+  Query: {
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: (root, args) => {
+      // TODO: args author and genre
+      return Book.find({})
+    },
+    allAuthors: () => {
+      return Author.find({})
+    }
+  },
+  Author: {
+    bookCount: (root) => {
+      // TODO
+      // const total = books.reduce((acc, current) => {
+      //   if (current.author === root.name) {
+      //     return acc + 1
+      //   }
+      //   return acc
+      // }, 0)
+      // return total
+    }
+  },
+  Book: {
+    author: (root) => {
+      // TODO
+    }
+  },
+  Mutation: {
+    addBook: async (root, args) => {
+      const book = new Book({
+        title: args.title,
+        published: args.published,
+        genres: args.genres
+      })
+      
+      let author = await Author.findOne({ name: args.author })
+      
+      if (!author) {
+        author = new Author({
+          name: args.author
+        })
+
+        await author.save()
+      }
+      
+      book.author = author
+
+      await book.save()
+      return book
+    },
+    editAuthor: (root, args) => {
+      // TODO
+      // const author = authors.find(a => a.name === args.name)
+      // if (!author) {
+      //   return null
+      // }
+
+      // const updatedAuthor = { ...author, born: args.setBornTo }
+      // authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      // return updatedAuthor
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Sever ready at ${url}`)
+})
+
+// TODO: Remove!
 
 let authors = [
   {
@@ -84,102 +219,3 @@ let books = [
     genres: ['classic', 'revolution']
   },
 ]
-
-const typeDefs = gql`
-  type Author {
-    name: String!
-    id: ID!
-    born: Int
-    bookCount: Int!
-  }
-
-  type Book {
-    title: String!
-    published: Int!
-    author: String!
-    id: ID!
-    genres: [String!]!
-  }
-
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(
-      author: String
-      genre: String
-    ): [Book!]!
-    allAuthors: [Author!]!
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      author: String!
-      published: Int!
-      genres: [String!]!
-    ): Book!
-    editAuthor(
-      name: String!
-      setBornTo: Int!
-    ): Author
-  }
-`
-
-const resolvers = {
-  Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      let filteredBooks = books
-      if (args.author) {
-        filteredBooks = filteredBooks.filter(book => book.author === args.author)
-      }
-      if (args.genre) {
-        filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre))
-      }
-      return filteredBooks
-    },
-    allAuthors: () => authors
-  },
-  Author: {
-    bookCount: (root) => {
-      const total = books.reduce((acc, current) => {
-        if (current.author === root.name) {
-          return acc + 1
-        }
-        return acc
-      }, 0)
-      return total
-    }
-  },
-  Mutation: {
-    addBook: (root, args) => {
-      if (!authors.find(a => a.name === args.author)) {
-        const newAuthor = { name: args.author, id: uuid() }
-        authors = authors.concat(newAuthor)
-      }
-      const newBook = { ...args, id: uuid() }
-      books = books.concat(newBook)
-      return newBook
-    },
-    editAuthor: (root, args) => {
-      const author = authors.find(a => a.name === args.name)
-      if (!author) {
-        return null
-      }
-
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
-      return updatedAuthor
-    }
-  }
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-
-server.listen().then(({ url }) => {
-  console.log(`Sever ready at ${url}`)
-})
